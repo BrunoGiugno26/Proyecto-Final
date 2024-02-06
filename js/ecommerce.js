2fea0d41846b6cf3ee376ca274672333690f8e04
BrunoGiugno26 <brunogiugno@gmail.com>
2024-02-06 13:50:52 -0300

    carrito de compras ya funcional con javascript

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -1,4 +1,4 @@
-// Funcionamiento del html ecommerce
+//?Funcionamiento del html ecommerce
 
 // Para abrir y cerrar el carrito
 
@@ -34,14 +34,14 @@ function start(){
 
 function update(){
     addEvents();
-    updateToal();
+    updateTotal();
 }
 
 // Eventos en funcion al añadido o borrado de los productos
 function addEvents(){
     // Quitar articulos del carrito
 
-    let cartRemove_btns = document.querySelectorAll(".cart-remove")
+    let cartRemove_btns = document.querySelectorAll(".cart-remove");
 
     console.log(cartRemove_btns);
 
@@ -52,7 +52,7 @@ function addEvents(){
 
 // Cambiar cantidad de Articulos
 
-let cartQuantity_inputs = document.querySelector(".cart-quantity")
+let cartQuantity_inputs = document.querySelectorAll(".cart-quantity");
 
 cartQuantity_inputs.forEach((input) => {
     input.addEventListener("change",handle_changeItemQuantity);
@@ -65,4 +65,126 @@ let addCart_btns = document.querySelectorAll(".add-cart");
 addCart_btns.forEach((btn) =>{
     btn.addEventListener("click",handle_addCartItem);
 });
+}
+
+// Comprar Orden
+
+const buy_btn = document.querySelector(".btn-buy");
+buy_btn.addEventListener("click",handle_buyOrden);
+
+// Ahora le doy funcionamiento al manejo de los eventos
+
+let itemsAdded = [];
+
+function handle_addCartItem(){
+    let product = this.parentElement;
+    let title = product.querySelector(".product-title").innerHTML;
+    let price = product.querySelector(".product-price").innerHTML;
+    let imgSrc = product.querySelector(".product-img").src;
+
+    console.log(title,price,imgSrc);
+
+
+    let newToAdd = {
+        title,
+        price,
+        imgSrc,
+    };
+
+    // El elemento de manejo ya existente
+
+    if(itemsAdded.find((el) => el.title == newToAdd.title)){
+        alert("Este Articulo ya existe");
+        return;
+    }else{
+        itemsAdded.push(newToAdd);
+    }
+
+    // Añadir Productos al carrito
+
+    let cartBoxElement = cartBoxComponent(title,price,imgSrc);
+    let newNode = document.createElement("div");
+    newNode.innerHTML = cartBoxElement;
+    const cartContent = cart.querySelector(".cart-content");
+    cartContent.appendChild(newNode);
+
+    update();
+}
+
+function handle_removeCartItem(){
+    this.parentElement.remove();
+
+    itemsAded = itemsAded.filter(
+        (el) =>
+        el.title != this.parentElement.querySelector(".cart-product-title").innerHTML
+    );
+
+    update();
+}
+
+function handle_changeItemQuantity(){
+    if(isNaN(this.value) || this.value < 1){
+        this.value = 1;
+    }
+    this.value = Math.floor(this.value); // Para mantener el numero entero y no quede ne decimal
+
+    update();
+}
+
+function handle_buyOrden(){
+    if(itemsAdded.length <= 0){
+        alert("Aun no hay ningun pedido para realizar! \nPor favor, haga un pedido primero");
+        return;
+    }
+
+    const cartContent = cart.querySelector(".cart-content");
+    cartContent.innerHTML = "";
+    alert("Su pedido se realizo con exito :)");
+    itemsAdded = [];
+    update();
+}
+
+// Funciones de Actualizar y Renderizar
+
+function updateTotal(){
+    let cartBoxes = document.querySelectorAll(".cart-box");
+    const totalElement = cart.querySelector(".total-price");
+    let total = 0;
+
+    cartBoxes.forEach((cartBox) =>{
+        let priceElement = cartBox.querySelector(".cart-price");
+        let price = parseFloat (priceElement.innerHTML.replace("$",""));
+        let quantity = cartBox.querySelector(".cart-quantity").value;
+
+        total += price * quantity;
+    });
+
+    total = total.toFixed(2);
+    // Quiere decir que quiero mantener 2 digitos despues del punto decimal
+
+    totalElement.innerHTML = "$" + total;
+}
+
+//  =========== Componentes HTML ============
+
+function cartBoxComponent(title,price,imgSrc){
+    return `
+    <div class="cart-box">
+
+    <img src=${imgSrc} alt ="" class="cart-img">
+
+    <div class="detail-box">
+
+    <div class ="cart-product-title">${title}</div>
+
+    <div class="cart-price">${price}</div>
+    <input type="number" value="1" class="cart-quantity">
+
+    </div>
+
+    <!___Eliminar cart ___>
+    <i class="bx bxs-trash-alt cart-remove"></i>
+
+    </div>
+    `;
 }
